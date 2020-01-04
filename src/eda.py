cf3b88553c80076277f36f73bfb0e2a92abe49c6
Abel Desta <abeld@ksu.edu>
2020-01-03 19:02:22 -0700

    plots and gitignore file

--- src/eda.py
+++ src/eda.py
@@ -52,6 +52,11 @@ if __name__ == "__main__":
     top20_nyc_neighborhoods = neighborhood_listing_count[:20]
     top20_labels = top20_nyc_neighborhoods.index
 
+    type_listings_nyc = nyc.groupby(['room_type', 'neighbourhood']).agg({'room_type':'count'})
+    top20_entire_home_count = [type_listings_nyc.loc[('Entire home/apt', i)]['room_type'] for i in top20_labels]
+    top20_private_room_count = [type_listings_nyc.loc[('Private room', i)]['room_type'] for i in top20_labels]
+    top20_shared_room_count = [type_listings_nyc.loc[('Shared room', i)]['room_type'] for i in top20_labels]
+
     ##Bar plot
     fig, ax = plt.subplots(1,1)
     width = 0.8
@@ -59,27 +64,40 @@ if __name__ == "__main__":
     ax.barh(tickLocals, top20_nyc_neighborhoods[::-1], width)
     ax.set_yticks(ticks = tickLocals)
     ax.set_yticklabels(top20_labels[::-1])
-    #ax.set_xlim(min(tickLocals)-0.6, max(tickLocals) + 0.6)
     plt.tight_layout(pad=1)
     plt.savefig('images/top20_neighbourhood_nyc.png')
 
-
+    ##Side by side bar chart 
+    fig, ax = plt.subplots(1,1, figsize = (12, 6))
+    width = 0.2
+    tickLocals = np.arange(10)
+    ax.bar(tickLocals-2*width, top20_entire_home_count[:10], width, color = 'red', label = 'Entire Home/Apt')
+    ax.bar(tickLocals-width, top20_private_room_count[:10], width, color = 'blue', label = 'Private room')
+    ax.bar(tickLocals, top20_shared_room_count[:10], width, color = 'green', label = 'Shared room')
+    ax.set_xticks(ticks = tickLocals)
+    ax.set_xticklabels(top20_labels[:10])
+    plt.xticks(rotation = 80)
+    plt.legend()
+    plt.tight_layout(pad=1)
+    plt.savefig('images/top20_room_type.png')
     
-    # samp = Sampling(data = nyc, column_name = 'price')
-    # random.seed(53194)
-    # boot_var = samp.bootstrap(20000)
+    samp = Sampling(data = nyc, column_name = 'price')
+    random.seed(53194)
+    boot_var = samp.bootstrap(20000)
     
-    # ##Bootstrap sample variance of NYC price
-    # fig, ax = plt.subplots(1, 1)
-    # ax.hist(boot_var, bins = 50)
-    # ax.set_title('Bootstrap Sample Variance in NYC Listing Price (20000 samples)')
-    # plt.savefig('images/bootstapVar_Hist.png')
+    ##Bootstrap sample variance of NYC price
+    fig, ax = plt.subplots(1, 1)
+    ax.hist(boot_var, bins = 50)
+    ax.set_title('Bootstrap Sample Variance in NYC Listing Price (20000 samples)')
+    plt.savefig('images/bootstapVar_Hist.png')
 
-    # ##Sample distribution of the mean price in NYC
-    # means = samp.sample_means(20000, 1000)
-    # fig, ax = plt.subplots(1, 1)
-    # ax.hist(means, bins = 50)
-    # ax.set_title('Sample Means in NYC Listing Price (20000 samples)')
-    # plt.savefig('images/sample_means_hist.png')
+    ##Sample distribution of the mean price in NYC
+    means = samp.sample_means(20000, 1000)
+    fig, ax = plt.subplots(1, 1)
+    ax.hist(means, bins = 50)
+    ax.set_title('Sample Means in NYC Listing Price (20000 samples)')
+    plt.savefig('images/sample_means_hist.png')
+
+    #Hypothesis Testing for Mean Price of sample vs population 
 
 
